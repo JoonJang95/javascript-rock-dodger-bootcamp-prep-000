03e41f47b0092ec0e0e07efebbd6453879b2abf1
Joon Jang <joon.jang95@gmail.com>
2018-09-13 19:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const LEFT_ARROW = 37 // use e.which!
 const RIGHT_ARROW = 39 // use e.which!
 const ROCKS = []
 const START = document.getElementById('start')
-START.innerHTML = "Joon\'s Game 오예~ (START)"
+START.innerHTML = "Joon\'s Game 오예~~ (START)"
 
 var gameInterval = null
 
